f7dc65cd563c150da3c281c84fd2a232174e172c
mcsavvy <phishink5@gmail.com>
2022-12-07 22:13:32 +0100

    0x04: Completed Task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key, val in a_dictionary.items():
+    if not isinstance(a_dictionary, dict):
+        return None
+    for key, val in tuple(a_dictionary.items()):
         if val == value:
             del a_dictionary[key]
     return a_dictionary
