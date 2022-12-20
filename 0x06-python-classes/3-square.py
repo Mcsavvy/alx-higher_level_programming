2f27a1fc4adaff1960c1110e7dbee33e13777cb3
mcsavvy <phishink5@gmail.com>
2022-12-20 23:45:31 +0100

    0x06: Completed Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+1. Square with size validation
+    Contains a 'Square' class that defines a square which accepts
+    a size at instantiation and provides a function for calculating
+    it's area.
+"""
+
+
+class Square:
+    """
+    An square class that accepts a size
+    """
+
+    def __init__(self, size=0):
+        """
+        initialize class with size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        calculate the area of a square
+        """
+        return self.__size ** 2
