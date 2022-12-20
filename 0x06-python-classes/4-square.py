eccf71384fbcea954c2e48d5b3d2cc854760aa02
mcsavvy <phishink5@gmail.com>
2022-12-20 23:50:29 +0100

    0x06: Completed Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+1. Square with size validation
+    Contains a 'Square' class that defines a square which accepts
+    a size at instantiation and provides a function for calculating
+    it's area.
+"""
+
+
+class Square:
+    """
+    An square class that accepts a size
+    """
+
+    def __init__(self, size=0):
+        """
+        initialize class with size
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        get size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        set size of square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        calculate the area of a square
+        """
+        return self.__size ** 2
