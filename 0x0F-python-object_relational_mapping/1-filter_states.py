c139a43d2b82313dd9c977425166a0f573438218
mcsavvy <phishink5@gmail.com>
2023-03-22 17:08:33 +0100

    Add script to filter states by name starting with 'N' and print results in ascending order

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A script that lists all states in a database."""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username, password, name = sys.argv[1:]
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=username,
+        passwd=password, db=name
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    for state in cur.fetchall():
+        print(state)
+    cur.close()
+    db.close()
