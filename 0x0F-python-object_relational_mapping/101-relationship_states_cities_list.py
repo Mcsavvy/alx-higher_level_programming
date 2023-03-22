220e75046eefd1a5b1a443ec66614f2fc00e1a5c
mcsavvy <phishink5@gmail.com>
2023-03-22 17:12:04 +0100

    List all states in the database and their corresponding cities in ascending order

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+A script that lists all states in the database.
+"""
+
+from relationship_city import Base, City
+from relationship_state import State
+
+
+if __name__ == "__main__":
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    import sys
+
+    host, password, name = sys.argv[1:]
+    uri = f"mysql+mysqldb://{host}:{password}@localhost/{name}"
+    engine = create_engine(uri, pool_pre_ping=True)
+
+    session = sessionmaker(bind=engine)()
+    query = (
+        session.query(State).join(City)
+        .order_by(State.id, City.id)
+    )
+
+    for state in query:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"\t{city.id}: {city.name}")
