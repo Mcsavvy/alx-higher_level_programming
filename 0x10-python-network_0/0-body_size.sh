db6877b8a7757e5dea2ebeefae9521b1bb7d50ef
mcsavvy <phishink5@gmail.com>
2023-03-31 06:00:14 +0100

    Refactor bash scripts for HTTP response handling and print only successful ones

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #/bin/bash
 # printing the size of an http response
 echo $(curl -sI $1 | grep 'content-length' | cut -d ' ' --fields=2)
-
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # a script that prints the body of a successful request
-status_code="$(curl -sI $1 | head -1 | cut -d ' ' --fields=2)"
-if [ $status_code == '200' ]; then
-	echo "$(curl -s $1)"
-fi
+if [ "$(curl -sI $1 | head -1 | cut -d ' ' --fields=2)" == '200' ]; then echo "$(curl -s $1)"; fi
