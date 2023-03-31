d334deac7e6cca926c4ce79e33b8c90db212647a
mcsavvy <phishink5@gmail.com>
2023-03-31 08:53:36 +0100

    Fix shebang in 0-body_size.sh script to ensure proper execution

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 This file contains a function that gets the peak number in a list
 """
 
+
 def find_peak(nums):
     """
     find the peak number in an array
@@ -27,7 +28,7 @@ def find_peak(nums):
     if nums[mid] >= left[-2] and nums[mid] >= right[1]:
         return nums[mid]
     if mid < left[-2]:
-        pl = find_peak(left)  
+        pl = find_peak(left)
         if pl is not None:
             return pl
         pr = find_peak(right)
