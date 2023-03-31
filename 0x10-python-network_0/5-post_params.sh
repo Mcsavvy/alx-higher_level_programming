ca7b6139bf8410259f83b908f9b08137af700527
mcsavvy <phishink5@gmail.com>
2023-03-31 09:33:00 +0100

    Add bash scripts to handle HTTP requests and responses

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -b "email=test@gmail.com&subject=I will always be here for PLD" -sX POST "$1"
+curl -d "email=test@gmail.com&subject=I will always be here for PLD" -sX POST "$1"
